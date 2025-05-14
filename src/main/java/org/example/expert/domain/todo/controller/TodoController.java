8c58d250769dd312d94ab8634ac2a78a15fce333
exmrim <exmrim@naver.com>
2025-05-14 18:44:38 +0900

    Feature: - Lv1-3 - Lv3-10 (TodoController)

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -5,12 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.example.expert.domain.common.annotation.Auth;
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
+import org.example.expert.domain.todo.dto.response.TodoCustomResponse;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -28,14 +31,29 @@ public class TodoController {
 
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
+            @RequestParam String weather, //Lv1-3. 코드 개선 퀴즈 -  JPA의 이해
+            @RequestParam LocalDateTime startedAt, //Lv1-3. 코드 개선 퀴즈 -  JPA의 이해
+            @RequestParam LocalDateTime endedAt, //Lv1-3. 코드 개선 퀴즈 -  JPA의 이해
             @RequestParam(defaultValue = "1") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        return ResponseEntity.ok(todoService.getTodos(weather, startedAt, endedAt, page, size));
     }
 
     @GetMapping("/todos/{todoId}")
     public ResponseEntity<TodoResponse> getTodo(@PathVariable long todoId) {
         return ResponseEntity.ok(todoService.getTodo(todoId));
+    }
+
+    //Lv3-10. QueryDSL 을 사용하여 검색 기능 만들기
+    @GetMapping("/todosCustom")
+    public ResponseEntity<Page<TodoCustomResponse>> getTodosCustom(
+            @RequestParam String title,
+            @RequestParam LocalDateTime createdAt,
+            @RequestParam String nickname,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ResponseEntity.ok(todoService.getTodosCustom(title, createdAt, nickname, page, size));
     }
 }
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,12 +7,26 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoQueryRepositoryCustom {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+
+    //Lv1-3. 코드 개선 퀴즈 -  JPA의 이해
+    @Query("SELECT t FROM Todo t "+
+            "LEFT JOIN FETCH t.user u " +
+            "WHERE (:weather IS NULL OR t.weather = :weather) " +
+            "AND (:startedAt IS NULL OR t.modifiedAt >= :startedAt) " +
+            "AND (:endedAt IS NULL OR t.modifiedAt <= :endedAt) " +
+            "ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByOrderByModifiedAtDesc(
+            @Param("weather") String weather,
+            @Param("startedAt") LocalDateTime startedAt,
+            @Param("endedAt") LocalDateTime endedAt,
+            Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
